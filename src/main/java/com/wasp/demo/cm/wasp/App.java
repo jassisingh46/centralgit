f658d58bac223b916a793957d95d69cc659d36d3
jasvinder singh <singhjassi46@gmail.com>
2021-07-24 20:13:55 +0530

    initialCode

---
+++ src/main/java/com/wasp/demo/cm/wasp/App.java
@@ -0,0 +1,13 @@
+package com.wasp.demo.cm.wasp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
